33580949354eaf40f4ee3e571c348da2e4d0ce4c
masproo22 <m.abbaszadeh2002@gmail.com>
2025-04-10 22:52:39 +0330

    feat: add footer component to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import FooterComp from "@/components/layout/footer/footerComp";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <FooterComp />
       </body>
     </html>
   );
