7eab570c1727b88e7ac423f524fd0bac32c8c712
Aziz Maghsoumi <azizmasooni1@gmail.com>
2025-04-12 20:34:18 +0330

    try to fix

--- src/components/layout/body/bodyComp.js
+++ src/components/layout/body/bodyComp.js
@@ -1,11 +1,3 @@
-import NewProducts from "@/components/ui/newProducts/newProducts"
-import NewSets from "@/components/ui/NewSets/NewSets"
-
 export default function BodyComp() {
-    return (
-        <>
-            < NewProducts />
-            <NewSets />
-        </>
-    )
+
 }
