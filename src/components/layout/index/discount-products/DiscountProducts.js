0a27e5029afaabcef5b4d2140e22e12b224a56fd
parisan <parisan0487@gmail.com>
2025-05-02 23:52:49 +0330

    new

--- src/components/layout/index/discount-products/DiscountProducts.js
+++ src/components/layout/index/discount-products/DiscountProducts.js
@@ -1,19 +1,18 @@
-"use client"
+'use client';
 
-import ProductCard from '@/components/ui/ProductCard'
+import ProductCard from '@/components/ui/ProductCard';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import MiniLoading from '../../loading/miniLoading';
-
+import MiniLoading from '../../loading/MiniLoading';
 
 const timer = [
     { label: 'ثانیه', value: 23 },
     { label: 'دقیقه', value: 28 },
     { label: 'ساعت', value: 13 },
     { label: 'روز', value: 15 },
-]
+];
 const DiscountProducts = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -23,7 +22,7 @@ const DiscountProducts = () => {
             try {
                 const res = await fetch('https://back-production-22f1.up.railway.app/api/products/');
                 const data = await res.json();
-                const discounted = data.filter(product => product.discount); // فقط اونایی که تخفیف دارن
+                const discounted = data.filter((product) => product.discount); // فقط اونایی که تخفیف دارن
                 setProducts(discounted);
                 setLoading(false);
             } catch (error) {
@@ -34,15 +33,13 @@ const DiscountProducts = () => {
         fetchProducts();
     }, []);
 
-
-
     const [timeLeft, setTimeLeft] = useState(() => {
-        const totalSeconds = (15 * 24 * 60 * 60) + (13 * 60 * 60) + (28 * 60) + 23;
+        const totalSeconds = 15 * 24 * 60 * 60 + 13 * 60 * 60 + 28 * 60 + 23;
         return totalSeconds;
     });
     useEffect(() => {
         const interval = setInterval(() => {
-            setTimeLeft(prev => prev > 0 ? prev - 1 : 0);
+            setTimeLeft((prev) => (prev > 0 ? prev - 1 : 0));
         }, 1000);
 
         return () => clearInterval(interval);
@@ -69,7 +66,7 @@ const DiscountProducts = () => {
     return (
         <>
             {/* //////////////// */}
-            < div className="relative flex-col items-center justify-center flex md:hidden w-full mx-2" >
+            <div className="relative flex-col items-center justify-center flex md:hidden w-full mx-2">
                 <Image
                     src="/assets/images/hero-bg.svg"
                     alt="Hero background"
@@ -78,15 +75,13 @@ const DiscountProducts = () => {
                     width={38}
                     height={38}
                     className="z-0 w-[15rem] h-[15rem] absolute  top-0"
-
                 />
                 <div className="z-10 flex flex-col items-center justify-center">
-                    <h3 className='text-[#FD5504] text-2xl md:text-3xl font-[800] mt-10 mx-5'>تخفیف های شگفت انگیز</h3>
+                    <h3 className="text-[#FD5504] text-2xl md:text-3xl font-[800] mt-10 mx-5">تخفیف های شگفت انگیز</h3>
                 </div>
-            </ div>
+            </div>
             <div className="rounded-3xl py-6 mb-[12rem] mt-8 w-full flex flex-col-reverse md:flex-row ">
-                <div className='w-full md:w-9/12 bg-[#FD5504] h-[18rem] rounded-[0_0_2rem_2rem] md:rounded-[2rem_0_2rem_2rem] '>
-
+                <div className="w-full md:w-9/12 bg-[#FD5504] h-[18rem] rounded-[0_0_2rem_2rem] md:rounded-[2rem_0_2rem_2rem] ">
                     <div className="relative w-full  p-3">
                         <Swiper
                             loop={true}
@@ -97,47 +92,46 @@ const DiscountProducts = () => {
                             breakpoints={{
                                 0: {
                                     slidesPerView: 1.1,
-                                    spaceBetween: 2
+                                    spaceBetween: 2,
                                 },
                                 360: {
                                     slidesPerView: 1.2,
-                                    spaceBetween: 2
+                                    spaceBetween: 2,
                                 },
                                 768: {
                                     slidesPerView: 2.3,
-                                    spaceBetween: 3
+                                    spaceBetween: 3,
                                 },
                                 930: {
                                     slidesPerView: 2.5,
-                                    spaceBetween: 3
+                                    spaceBetween: 3,
                                 },
 
                                 1224: {
                                     slidesPerView: 2.7,
-                                    spaceBetween: 3
+                                    spaceBetween: 3,
                                 },
 
                                 1350: {
                                     slidesPerView: 3.5,
-                                    spaceBetween: 3
+                                    spaceBetween: 3,
                                 },
                                 1550: {
                                     slidesPerView: 4,
-                                    spaceBetween: 3
+                                    spaceBetween: 3,
                                 },
-
                             }}
                         >
                             {products.map((product) => (
-                                <SwiperSlide key={product._id} >
+                                <SwiperSlide key={product._id}>
                                     <ProductCard data={product} key={product.id} thumbnails={false} />
                                 </SwiperSlide>
                             ))}
                         </Swiper>
                     </div>
                 </div>
-                <div className='w-full md:w-3/12 '>
-                    <div className='h-[9rem] bg-[#FD5504] rounded-[2rem_2rem_0_0] md:rounded-[0_2rem_2rem_0]'>
+                <div className="w-full md:w-3/12 ">
+                    <div className="h-[9rem] bg-[#FD5504] rounded-[2rem_2rem_0_0] md:rounded-[0_2rem_2rem_0]">
                         <div className="flex justify-center items-center gap-2  p-4 rounded-xl w-full">
                             {timer.map((item, index) => (
                                 <div
@@ -151,12 +145,28 @@ const DiscountProducts = () => {
                                 </div>
                             ))}
                         </div>
-
                     </div>
                     {/* ////////// */}
-                    <div className='hidden md:block absolute rotate-90 translate-x-[-1px]'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="38" viewBox="0 0 40 38" fill="#FD5504"><g clipPath="url(#clip0_263_127)"><path d="M-648 -51C-648 -73.0914 -630.091 -91 -608 -91H-40C-17.9086 -91 0 -73.0914 0 -51V-2C0 20.0914 17.9086 38 40 38H608C630.091 38 648 55.9086 648 78V206C648 228.091 630.091 246 608 246H-608C-630.091 246 -648 228.091 -648 206V-51Z" fill="var(--mi-color-secondary)"></path></g><defs><clipPath id="clip0_263_127"><rect x="-648" y="-91" width="1296" height="337" rx="32" fill="none"></rect></clipPath></defs></svg>
-
+                    <div className="hidden md:block absolute rotate-90 translate-x-[-1px]">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="40"
+                            height="38"
+                            viewBox="0 0 40 38"
+                            fill="#FD5504"
+                        >
+                            <g clipPath="url(#clip0_263_127)">
+                                <path
+                                    d="M-648 -51C-648 -73.0914 -630.091 -91 -608 -91H-40C-17.9086 -91 0 -73.0914 0 -51V-2C0 20.0914 17.9086 38 40 38H608C630.091 38 648 55.9086 648 78V206C648 228.091 630.091 246 608 246H-608C-630.091 246 -648 228.091 -648 206V-51Z"
+                                    fill="var(--mi-color-secondary)"
+                                ></path>
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_263_127">
+                                    <rect x="-648" y="-91" width="1296" height="337" rx="32" fill="none"></rect>
+                                </clipPath>
+                            </defs>
+                        </svg>
                     </div>
                     {/* //////////////// */}
                     <div className="relative  flex-col items-center justify-center hidden md:flex">
@@ -168,22 +178,27 @@ const DiscountProducts = () => {
                             width={38}
                             height={38}
                             className="z-0 w-[14rem] h-[14rem] absolute  top-0"
-
                         />
                         <div className="z-10 flex flex-col items-center justify-center">
-                            <h3 className='text-[#FD5504] text-2xl font-[800] mt-10 mx-5'>تخفیف های شگفت انگیز</h3>
-                            <Link href={""} className="text-md text-nowrap  border rounded-2xl text-center px-6  py-3 mt-3 text-[#BABABA] font-[500] cursor-pointer">
+                            <h3 className="text-[#FD5504] text-2xl font-[800] mt-10 mx-5">تخفیف های شگفت انگیز</h3>
+                            <Link
+                                href={''}
+                                className="text-md text-nowrap  border rounded-2xl text-center px-6  py-3 mt-3 text-[#BABABA] font-[500] cursor-pointer"
+                            >
                                 مشاهدهٔ همه
                             </Link>
                         </div>
                     </div>
                 </div>
-            </ div>
-            <Link href={""} className="text-md block md:hidden text-nowrap  border rounded-2xl text-center px-6  py-3 mt-3 mb-12 text-[#BABABA] font-[500] cursor-pointer">
+            </div>
+            <Link
+                href={''}
+                className="text-md block md:hidden text-nowrap  border rounded-2xl text-center px-6  py-3 mt-3 mb-12 text-[#BABABA] font-[500] cursor-pointer"
+            >
                 مشاهدهٔ همه
             </Link>
         </>
-    )
-}
+    );
+};
 
-export default DiscountProducts
+export default DiscountProducts;
--- src/components/layout/index/new-sets/NewSets.js
+++ src/components/layout/index/new-sets/NewSets.js
@@ -6,8 +6,7 @@ import 'swiper/css';
 import ImageSlider from './ImageSlider';
 import slideToleftI from 'assets/images/slide-arrow-1.svg';
 import slideToRithI from 'assets/images/slide-arrow.svg';
-import MiniLoading from '../../loading/miniLoading';
-
+import MiniLoading from '../../loading/MiniLoading';
 
 const NewSets = () => {
     const [centerIndex, setCenterIndex] = useState(0);
@@ -22,9 +21,7 @@ const NewSets = () => {
                 const data = await res.json();
 
                 // فقط محصولاتی که category شامل "ست" هست رو نگه می‌داریم
-                const filtered = data.filter(product =>
-                    product.categories.includes('ست')
-                );
+                const filtered = data.filter((product) => product.categories.includes('ست'));
 
                 setProducts(filtered);
                 setLoading(false);
@@ -35,7 +32,6 @@ const NewSets = () => {
 
         fetchProducts();
     }, []);
-
 
     useEffect(() => {
         const handleResize = () => {
@@ -71,7 +67,6 @@ const NewSets = () => {
 
     return (
         <div className="flex flex-col items-center py-10 bg-[#F7F7F7] text-[#2D2929] w-full">
-
             <h2 className="text-2xl mt-8">
                 <span className="font-black">جدیدترین</span> ست‌ها
             </h2>
@@ -89,10 +84,7 @@ const NewSets = () => {
             </div>
 
             <div className="relative w-full max-w-[1300px] flex items-center justify-center mt-12">
-                <button
-                    onClick={goPrev}
-                    className="absolute left-2 z-10 bg-white p-2 rounded-xl shadow cursor-pointer"
-                >
+                <button onClick={goPrev} className="absolute left-2 z-10 bg-white p-2 rounded-xl shadow cursor-pointer">
                     <Image src={slideToleftI} alt="prev" width={24} height={24} />
                 </button>
 
@@ -100,10 +92,11 @@ const NewSets = () => {
                     {visibleItems.map((item, index) => (
                         <div
                             key={item._id + index}
-                            className={`transition-all duration-400 ${item.isCenter
-                                ? 'w-[18rem] md:w-[25rem]'  // وسطی بزرگ
-                                : 'w-[7rem] md:w-[10rem]'   // کناریا کوچیک
-                                }`}
+                            className={`transition-all duration-400 ${
+                                item.isCenter
+                                    ? 'w-[18rem] md:w-[25rem]' // وسطی بزرگ
+                                    : 'w-[7rem] md:w-[10rem]' // کناریا کوچیک
+                            }`}
                         >
                             {item.isCenter ? (
                                 <ImageSlider items={item} />
@@ -117,9 +110,6 @@ const NewSets = () => {
                         </div>
                     ))}
                 </div>
-
-
-
 
                 <button
                     onClick={goNext}
